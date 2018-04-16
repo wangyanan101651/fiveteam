aa2813ef9f9000720e40bce074bd2c26bc451b00
Your Name <you@example.com>
2018-04-16 21:28:14 +0800

    五组王亚楠的第一次提交

---
+++ blog_font/src/components/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <div>{{name}}</div>
+      <img src="/static/logo.png" alt="">
+  </div>
+      
+</template>
+<script>
+  export default{
+      data(){
+        return {name:"zhangsan"}
+      }
+  }
+</script>
+<style scoped>
+  div{
+    font-size: 50px;
+  }
+</style>
