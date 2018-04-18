5e2836ca1dd3d7493e952f538c4629840df833a0
wanghuixin065 <18810961948>
2018-04-18 08:45:35 +0800

    wanghuixin

--- blog_api/app.js
+++ blog_api/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var bodyParser = require('body-parser')
+var bodyParser=require("body-parser")
 
 
 var app = express();
@@ -12,34 +12,35 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser());
 
+app.use(express.static(path.join(__dirname, 'public')));
 
-//页面渲染
+
+// 页面渲染
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-//接口
+// 接口
 
-var apiUser = require('./api/back/user.js')
+
+var apiUser=require("./api/back/user.js")
 app.use('/api/back/user', apiUser);
+
+
 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
-
 
 // error handler
 app.use(function(err, req, res, next) {
