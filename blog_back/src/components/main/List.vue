b049b7c117c79e2bdfaf875080196d0ffbba0bfe
Your Name <you@example.com>
2018-04-19 08:46:11 +0800

    new

--- blog_back/src/components/main/List.vue
+++ blog_back/src/components/main/List.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-      颠三倒四多
+  <div class="classList">
+      <el-table style="width: 100%">
+      <el-table-column label="中文名称" width="180"> </el-table-column>
+      <el-table-column label="英文名称" width="180"> </el-table-column>
+      <el-table-column label="操作" width="180"> </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -11,5 +15,7 @@ export default {
 </script>
 
 <style>
-
+  .classList{
+    padding: 30px;
+  }
 </style>
