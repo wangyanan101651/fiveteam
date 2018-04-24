9abec0804be7c981249f7cde243f423d986cb687
wanghuixin065 <18810961948>
2018-04-24 08:48:09 +0800

    wanghuixin

--- blog_api/api/back/article.js
+++ blog_api/api/back/article.js
@@ -10,30 +10,22 @@ var {
   searchHandleNormal,  //检测有无某条数据，有为true
   query             // //基本操作
 }=require("../../config/db_connect")
-router.post("/ArticleClass", (req, res, next) => {
-  let { article_name, visitors, editer,content,time,daodu,recommend,art_show,enname_one } = req.body
-
-  if (article_name, visitors, editer,content,time,daodu,recommend,art_show,enname_one) {
-    let oneData = `alter table ${article_name} rename ${visitors}`
-    let twoData = `update bawei set enname='${visitors}',cnname='${editer}'`
-    async function sqlAllHandle() {
-      await sqlHandle(oneData);
-      await sqlHandle(twoData);
-      return {
-        code: "3001",
-        msg: "修改数据成功",
-      }
-    }
-    sqlAllHandle().then((data) => {
-      res.send(data)
-    }).catch((err) => {
-      res.send({
-        code: "3002",
-        msg: "修改数据失败"
-      })
+ 
+router.post("/updateArticle", (req, res, next) => {
+     
+  let sql = `update ${req.body.enname_one} set article_name='${req.body.article_name}', editer='${req.body.editer}', content='${req.body.content}', time='${req.body.time}', visitors='${req.body.visitors}', daodu='${req.body.daodu}', imgsrc='${req.body.imgsrc}', recommend='${req.body.recommend}', art_show='${req.body.art_show}' where id='${req.body.id}'` 
+  sqlHandle(sql).then((data)=>{
+    res.send({
+      code:'3201',
+      msg:'修改数据成功'
     })
-
-  }
+  }).catch((err)=>{
+    res.send({
+      code:'3202',
+      msg:'修改数据失败'
+    })
+  })  
+    
 })
 
 
@@ -42,28 +34,26 @@ router.post("/ArticleClass", (req, res, next) => {
 
 //后台文章删除一级接口
 router.post("/deleteArticle", (req, res, next) => {
-  let { id, enname_one } = req.body
-  if (id, enname_one) {
-    let sqlback = `delete from bawei where id='${id}'`
-    console.log(sqlback)
-    let deleteTable = `drop table ${enname_one}`
-    async function sqlAllHandle() {
-      await query(sqlone);
-      await query(deleteTable)
-      return {
-        code: "1225",
-        msg: "删除数据成功",
-      }
-    }
-    sqlAllHandle().then((data) => {
-      res.send(data)
-    }).catch((err) => {
-      res.send({
-        code: "1224",
-        msg: "删除数据失败"
-      })
-    })
+  let deletesql = `delete from ${req.body.enname_one} where id='${req.body.id}'`
+  var updateArticleNum = `update two_class set article_name=article_name-1 where id = '${req.body.twoId}'`
+
+  const asyncdeleteArticle = async function (){
+    await sqlHandle(deletesql)
+    await sqlHandle(updateArticleNum)
+    return 
   }
+  asyncdeleteArticle().then((data)=>{
+    res.send({
+      code:'3301',
+      msg:'删除数据成功'
+    })
+  }).catch((err)=>{
+    res.send({
+      code:'3302',
+      msg:'删除数据失败'
+    })
+  })  
 
 })
+
 module.exports = router
